5083ba600b8292d348fc11f2f2cc6f0caedb7c1b
noormohammad20 <mdsaim204@gmail.com>
2022-03-21 00:25:49 +0600

    how to create a json file

--- src/Components/shoes/Shoes.js
+++ src/Components/shoes/Shoes.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import { add, multiply } from './calculate'
+import Cosmetic from '../Cosmetic/Cosmetic'
+
 
 const Shoes = () => {
-    const first = 14
-    const second = 11
-    const result = multiply(first, second)
-    const sum = add(first, second)
+    const cosmetics = [
+        { id: 1, name: 'alta', price: 100 },
+        { id: 2, name: 'nail polish', price: 200 },
+        { id: 3, name: 'mashkara', price: 700 },
+        { id: 4, name: 'churi', price: 300 },
+        { id: 5, name: 'mehedi', price: 50 },
+
+    ]
     return (
         <div>
             <h2>This Is Shoes Component</h2>
-            <p>Result: {result} and total:{sum}</p>
+            {
+                cosmetics.map(cosmetic => <Cosmetic
+                    key={cosmetic.id}
+                    cosmetic={cosmetic}
+                ></Cosmetic>)
+            }
         </div>
     )
 }
